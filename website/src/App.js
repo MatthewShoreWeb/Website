3c48f276dd55f9ecac9af68872b7ac6ffd1040a8
matthewshoreweb <mazza1205@gmail.com>
2026-07-16 20:57:04 +0100

    React setup

---
+++ website/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id='appBody'>
+      <header id='appHeader'>
+        <nav>
+          Navigation
+        </nav>
+      </header>
+
+      <main id='appBody'>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
